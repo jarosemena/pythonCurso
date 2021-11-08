d7acb636906971bb30dd3f34a6151c68eeaa5011
Jose Jair <jjarosemena@gmail.com>
2021-11-08 05:54:37 -0500

    se crea funcion de multiplicar sin *

---
+++ ejercicios/multiplicar.py
@@ -0,0 +1,9 @@
+def multiplicar( a, b, resp = 0  ):   
+    if b == 0:
+       None
+    else:
+        #resp = resp + a
+        resp = multiplicar (a, b-1,resp) + a
+    return resp
+   
+print('la respuesta es= ', multiplicar( 3,3))
