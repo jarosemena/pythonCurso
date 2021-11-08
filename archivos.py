3dfd339b940cff74a2f132b23c98c6a9ac4fad96
Jose Jair <jjarosemena@gmail.com>
2021-11-07 20:29:44 -0500

    se mejoro el codigo  mostrar los  usos de archivos

--- archivos.py
+++ archivos.py
@@ -1,8 +1,33 @@
 file = open("textoprueba1.txt")
-file2 = open("textoprueba2.txt",'w')
-file3 = open("textoprueba3.txt", 'a')
+
 
 print(file.read())
 
+file.close
+file = open("textoprueba1.txt")
+
+for line in file:
+    print(line)
+
+file.close
+
+file2 = open("textoprueba2.txt",'w')
+file3 = open("textoprueba3.txt", 'a')
+
 file2.writelines('hola mundo')
 file3.writelines('hola mundo 3')
+file3.write('\nPrueba Hola Mundo linea nueva')
+
+
+
+file2.close
+file3.close
+
+file2 = open("textoprueba2.txt",'r')
+file3 = open("textoprueba3.txt", 'r')
+
+print('archivo 1',file2.read())
+print('archivo 2',file3.read())
+
+file2.close
+file3.close
